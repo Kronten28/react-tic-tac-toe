dc372919bacbdc0bf758f633185c30b34e0791c1
Kushal ⠀ <kush.s2805@gmail.com>
2023-07-02 23:23:46 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React, { Component }  from 'react';
+
+export default function Board() {
+  return (
+    <>
+    <div className='board-row'>
+      <button className="square">X</button>
+      <button className="square">X</button>
+      <button className="square">X</button>
+    </div>
+    <div className='board-row'>
+      <button className="square">X</button>
+      <button className="square">X</button>
+      <button className="square">X</button>
+    </div>
+    <div className='board-row'>
+      <button className="square">X</button>
+      <button className="square">X</button>
+      <button className="square">X</button>
+    </div>
+    </>
+  );
+}
